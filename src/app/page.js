01d4e9d104bf36a8cbc7e1bf8d854fe5b608328b
Pouya <pouyasamadnejad@gmail.com>
2024-09-22 02:18:21 +0330

    form added to main paget

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Description from "@/components/Description";
+import Form from "@/components/Form";
 import ImageSlider from "@/components/ImageSlider";
 import Products from "@/components/Products";
 import Title from "@/components/Title";
@@ -11,6 +12,8 @@ export default function Home() {
       <Products />
       <Title>درباره ما</Title>
       <Description></Description>
+      <Title>فرم تماس</Title>
+      <Form />
     </div>
   );
 }
